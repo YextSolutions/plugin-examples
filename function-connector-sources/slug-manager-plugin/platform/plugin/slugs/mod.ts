0cba32e0eb18b1a6e9c89dc5b5c162ad40d31a11
Dana Fesjian <dzfesjian@gmail.com>
2023-08-24 11:54:32 +0200

    adding slug manager connector and plugin

---
+++ function-connector-sources/slug-manager-plugin/platform/plugin/slugs/mod.ts
@@ -0,0 +1,13 @@
+import { createSlugManager } from "https://deno.land/x/yextpages@plugins@1.0.0-beta.3/mod.ts";
+
+declare const API_KEY: string;
+
+export const {webhook, connector} = createSlugManager({
+	apiKey: API_KEY,
+	slugFormat: "[[localeCode]]/[[address.region]]/[[address.city]]/[[address.line1]]",
+	slugFormatLocaleOverrides: {
+		"en": "[[address.region]]/[[address.city]]/[[address.line1]]"
+	},
+	entityTypes: ["location"],
+    //env: "sandbox"
+});
