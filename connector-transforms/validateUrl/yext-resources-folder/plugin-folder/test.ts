b4cb19e29d958cf1d9ab19d2498d26a6ef7decf4
racheladler <85689321+racheladler@users.noreply.github.com>
2024-02-15 15:18:28 -0500

    Delete connector-transforms/validateUrl/yext-resources-folder/plugin-folder directory

--- connector-transforms/validateUrl/yext-resources-folder/plugin-folder/test.ts
+++
@@ -1,6 +0,0 @@
-import { validateUrl } from "./mod.ts";
-Deno.test("Testing export function", async () => {
-    const url = 'https://developer.conga.com/manage-fulfill/reference/put_billingpreference-id-toggleactivestatus-activeflag'
-    const output = await validateUrl(url);
-  console.log('This is the output:', output)
-});
